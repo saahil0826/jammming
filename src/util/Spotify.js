8cfc660e59f1490e8da34ff04f7dd787957f74f2
saahil0826 <snawalkha0826@gmail.com>
2018-05-26 15:37:38 +0530

    2nd

---
+++ src/util/Spotify.js
@@ -0,0 +1,14 @@
+const apiKey = '';
+
+const clientId = ' 0851acd12944496398752b7378093876';
+
+const redirectURI = "http://localhost:3000/";
+
+
+const Spotify = {
+  getAccessToken(){
+
+  }
+};
+
+export default Spotify;
